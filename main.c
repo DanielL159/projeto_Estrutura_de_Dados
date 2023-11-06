6f9ab5a8575ab4dd2f96a6b42e2c6a3ded5d8d93
DanielL159 <danielluizdossantos12@gmail.com>
2023-11-06 15:12:10 -0300

    base

---
+++ main.c
@@ -0,0 +1,33 @@
+
+#include <stdio.h>
+#include "biblioteca.h"
+
+struct fornecedor
+{
+    char nome,endereço,email, telefone, endereço, UF;
+};
+struct cliente
+{
+    char nome, idade, email, telefone, endereço, UF;
+};
+
+int main (){
+
+/*Listar todos os tipos de serviços.*/
+
+/*Listar todas os clientes.*/
+
+/*Listar todos os prestadores de serviços.*/
+
+/*Listar os clientes de um determinado estado.*/
+
+/*Listar os prestadores serviço de um determinado tipo.*/
+
+/*Apresentar o(s) estado(s) onde está registrado o serviço mais caro.*/
+
+/*Listar todos os serviços em ordem crescente de valor.*/
+
+/*Listar todos os clientes em ordem crescente de nome.*/
+
+    return 1;
+}
