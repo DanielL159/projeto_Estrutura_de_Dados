edde057c6a20e37c6b7e9655d99557873d46406e
DanielL159 <danielluizdossantos12@gmail.com>
2023-11-16 09:16:07 -0300

    exclusao de arquivos

--- biblioteca_2.h
+++ biblioteca_2.h
@@ -1,135 +1,132 @@
+#ifndef BIBLIOTECA_H
+#define BIBLIOTECA_H
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 
 #define MAX_NOME 50
 #define MAX_EMAIL 50
 #define MAX_ENDERECO 50
 #define MAX_DETALHAMENTOSERV 100
 
-//declaraÃ§Ã£o das structs com caracterÃ­sticas especÃ­ficas de cada "tÃ³pico"
-
-/*typedef struct compra {
-};*/
-
-typedef struct{
+typedef struct {
     char detalhamento[MAX_DETALHAMENTOSERV];
     char dataCadastro[20];
     float preco;
-}Servico;
+} servico;
 
-typedef struct{
-    Servico produto;
+typedef struct {
+    servico produto;
     char nome[MAX_NOME];
     char endEletronico[MAX_ENDERECO];
     char email[MAX_EMAIL];
     int telefone;
     char endereco[MAX_ENDERECO];
-    char UF[4]; 
-}Prestador;
+    char UF[4];
+} prestador;
 
-typedef struct{
+typedef struct {
     char nome[MAX_NOME];
     int idade;
     char email[MAX_EMAIL];
     int telefone;
     char endereco[MAX_ENDERECO];
-    int UF;
-}Consumidor;
+    char UF[4];
+} consumidor;
 
-char *cadastrarUF(int opcao){
+char *cadastrarUF(int opcao) {
     static char UF[4];
 
-     switch (opcao) {
+    switch (opcao) {
         case 1:
-            strcpy(UF, "AC"); // Acre
+            strcpy(UF, "AC");
             break;
         case 2:
-            strcpy(UF, "AL"); // Alagoas
+            strcpy(UF, "AL");
             break;
         case 3:
-            strcpy(UF, "AP"); // AmapÃ¡
+            strcpy(UF, "AP");
             break;
         case 4:
-            strcpy(UF, "AM"); // Amazonas
+            strcpy(UF, "AM");
             break;
         case 5:
-            strcpy(UF, "BA"); // Bahia
+            strcpy(UF, "BA");
             break;
         case 6:
-            strcpy(UF, "CE"); // CearÃ¡
+            strcpy(UF, "CE");
             break;
         case 7:
-            strcpy(UF, "ES"); // EspÃ­rito Santo
+            strcpy(UF, "ES");
             break;
         case 8:
-            strcpy(UF, "GO"); // GoiÃ¡s
+            strcpy(UF, "GO");
             break;
         case 9:
-            strcpy(UF, "MA"); // MaranhÃ£o
+            strcpy(UF, "MA");
             break;
         case 10:
-            strcpy(UF, "MT"); // Mato Grosso
+            strcpy(UF, "MT");
             break;
         case 11:
-            strcpy(UF, "MS"); // Mato Grosso do Sul
+            strcpy(UF, "MS");
             break;
         case 12:
-            strcpy(UF, "MG"); // Minas Gerais
+            strcpy(UF, "MG");
             break;
         case 13:
-            strcpy(UF, "PA"); // ParÃ¡
+            strcpy(UF, "PA");
             break;
         case 14:
-            strcpy(UF, "PB"); // ParaÃ­ba
+            strcpy(UF, "PB");
             break;
         case 15:
-            strcpy(UF, "PR"); // ParanÃ¡
+            strcpy(UF, "PR");
             break;
         case 16:
-            strcpy(UF, "PE"); // Pernambuco
+            strcpy(UF, "PE");
             break;
         case 17:
-            strcpy(UF, "PI"); // PiauÃ­
+            strcpy(UF, "PI");
             break;
         case 18:
-            strcpy(UF, "RJ"); // Rio de Janeiro
+            strcpy(UF, "RJ");
             break;
         case 19:
-            strcpy(UF, "RN"); // Rio Grande do Norte
+            strcpy(UF, "RN");
             break;
         case 20:
-            strcpy(UF, "RS"); // Rio Grande do Sul
+            strcpy(UF, "RS");
             break;
         case 21:
-            strcpy(UF, "RO"); // RondÃ´nia
+            strcpy(UF, "RO");
             break;
         case 22:
-            strcpy(UF, "RR"); // Roraima
+            strcpy(UF, "RR");
             break;
         case 23:
-            strcpy(UF, "SC"); // Santa Catarina
+            strcpy(UF, "SC");
             break;
         case 24:
-            strcpy(UF, "SP"); // SÃ£o Paulo
+            strcpy(UF, "SP");
             break;
         case 25:
-            strcpy(UF, "SE"); // Sergipe
+            strcpy(UF, "SE");
             break;
         case 26:
-            strcpy(UF, "TO"); // Tocantins
+            strcpy(UF, "TO");
             break;
         default:
-            printf("OpÃ§Ã£o invÃ¡lida. Digite um nÃºmero de 1 a 26.\n");
+            printf("Opção inválida. Digite um número de 1 a 26.\n");
     }
     return UF;
 }
 
-void cadastrarPrestador(Prestador *industria) {
-    char x[4];
-    int opcao=0;
+void cadastrarPrestador(prestador *industria) {
+    int opcao = 0;
 
-    printf("Prestador\n____________");
+    printf("Prestador\n____________\n");
     printf("\nNOME: ");
     scanf("%s", (*industria).nome);
     fflush(stdin);
@@ -149,11 +146,52 @@ void cadastrarPrestador(Prestador *industria) {
     printf("ENDERECO: ");
     scanf("%s", &(*industria).endereco);
     fflush(stdin);
-    
+
     printf("UNIDADE FEDERATIVA:\n\n");
-    printf("[1]--> (AC)Acre\t\t[2]--> (AL)Alagoas\t\t[3]--> (AP)Amapa\t\t[4]--> (AM)Amazonas\n[5]--> (BA)Bahia\t[6]--> (CE)Ceara\t\t[7]--> (ES)Espirito Santo\t[8]--> (GO)Goias\n[9]--> (MA)Maranhao\t[10]--> (MT)Mato Grosso\t\t[11]--> (MS)Mato Grosso do Sul\t[12]--> (MG)Minas Gerais\n[13]--> (PA)Para\t[14]--> (PB)Paraiba\t\t[15]--> (PR)Parana\t\t[16]--> (PE)Pernambuco\n[17]--> (PI)Piaui\t[18]--> (RJ)Rio de Janeiro\t[19]--> (RN)Rio Grande do Norte\t[20]--> (RS)Rio Grande do Sul\n[21]--> (RO)Rondonia\t[22]--> (RR)Roraima\t\t[23]--> (SC)Santa Catarina\t[24]--> (SP)Sao Paulo\n[25]--> (SE)Sergipe\t[26]--> (TO)Tocantins\n\nDIGITE UMA UF:");
-    scanf("%d",&opcao);
+    printf("[1]--> (AC)Acre\t\t[2]--> (AL)Alagoas\t\t[3]--> (AP)Amapa\t\t[4]--> (AM)Amazonas\n"
+           "[5]--> (BA)Bahia\t[6]--> (CE)Ceara\t\t[7]--> (ES)Espirito Santo\t[8]--> (GO)Goias\n"
+           "[9]--> (MA)Maranhao\t[10]--> (MT)Mato Grosso\t\t[11]--> (MS)Mato Grosso do Sul\t[12]--> (MG)Minas Gerais\n"
+           "[13]--> (PA)Para\t[14]--> (PB)Paraiba\t\t[15]--> (PR)Parana\t\t[16]--> (PE)Pernambuco\n"
+           "[17]--> (PI)Piaui\t[18]--> (RJ)Rio de Janeiro\t[19]--> (RN)Rio Grande do Norte\t[20]--> (RS)Rio Grande do Sul\n"
+           "[21]--> (RO)Rondonia\t[22]--> (RR)Roraima\t\t[23]--> (SC)Santa Catarina\t[24]--> (SP)Sao Paulo\n"
+           "[25]--> (SE)Sergipe\t[26]--> (TO)Tocantins\n\nDIGITE UMA UF:");
+    scanf("%d", &opcao);
     strcpy((*industria).UF, cadastrarUF(opcao));
-    fflush(stdin);
 }
 
+void cadastrarConsumidor(consumidor *comprador) {
+    int opcao;
+
+    printf("Comprador\n__________\n");
+    printf("\nNOME: ");
+    scanf("%s", comprador->nome);
+    fflush(stdin);
+
+    printf("IDADE: ");
+    scanf("%d", &comprador->idade);
+    fflush(stdin);
+
+    printf("E-MAIL: ");
+    scanf("%s", comprador->email);
+    fflush(stdin);
+
+    printf("TELEFONE: ");
+    scanf("%d", &comprador->telefone);
+    fflush(stdin);
+
+    printf("ENDERECO: ");
+    scanf("%s", &comprador->endereco);
+    fflush(stdin);
+
+    printf("UNIDADE FEDERATIVA:\n\n");
+    printf("[1]--> (AC)Acre\t\t[2]--> (AL)Alagoas\t\t[3]--> (AP)Amapa\t\t[4]--> (AM)Amazonas\n"
+           "[5]--> (BA)Bahia\t[6]--> (CE)Ceara\t\t[7]--> (ES)Espirito Santo\t[8]--> (GO)Goias\n"
+           "[9]--> (MA)Maranhao\t[10]--> (MT)Mato Grosso\t\t[11]--> (MS)Mato Grosso do Sul\t[12]--> (MG)Minas Gerais\n"
+           "[13]--> (PA)Para\t[14]--> (PB)Paraiba\t\t[15]--> (PR)Parana\t\t[16]--> (PE)Pernambuco\n"
+           "[17]--> (PI)Piaui\t[18]--> (RJ)Rio de Janeiro\t[19]--> (RN)Rio Grande do Norte\t[20]--> (RS)Rio Grande do Sul\n"
+           "[21]--> (RO)Rondonia\t[22]--> (RR)Roraima\t\t[23]--> (SC)Santa Catarina\t[24]--> (SP)Sao Paulo\n"
+           "[25]--> (SE)Sergipe\t[26]--> (TO)Tocantins\n\nDIGITE UMA UF:");
+    scanf("%d", &opcao);
+    strcpy(comprador->UF, cadastrarUF(opcao));
+}
+#endif
--- main.c
+++ main.c
@@ -1,31 +1,29 @@
+#include "biblioteca_2.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "biblioteca_2.h"
 #include <time.h>
 
 #define MAX_NOME 50
 #define MAX_EMAIL 50
 #define MAX_ENDERECO 50
 #define MAX_DETALHAMENTOSERV 100
- 
-//declaraÃ§Ã£o das structs com caracterÃ­sticas especÃ­ficas de cada "tÃ³pico"
 
-/*typedef struct compra {
-};*/
-
-int main() {
+int main()
+{
+    int i = 0;
     int nPrestador = 0;
+    int nCliente = 0;
     prestador industria[100];
     consumidor cliente[100];
     int opcao;
-    int sair=0;
-	int i;
+    int sair = 0;
 
-    do {
+    do
+    {
         printf("\n\n********FARMACIA*********\n\n");
         printf("[1] Cadastrar prestador ou/e produto\n");
-        printf("[2] Cadastrar Cliente\n");
+        printf("[2] Cadastrar Cliente ou/e compra\n");
         printf("[3] Listar tipos de servicos\n");
         printf("[4] Listar clientes\n");
         printf("[5] Listar prestadores de servico\n");
@@ -39,23 +37,24 @@ int main() {
         printf("ESCOLHA UMA OPCAO: ");
         scanf("%d", &opcao);
 
-        switch (opcao) {
-            case 1:
-            i = 0;
-            do{ 
+        switch (opcao)
+        {
+        case 1:
+            do
+            {
                 cadastrarPrestador(&industria[i]);
+                nPrestador++;
 
-                printf("Deseja cadastrar um produto a empresa %s: (1)SIM (0)NAO",industria[i].produto.detalhamento);
-                scanf("%d",&opcao);
+                printf("Deseja cadastrar um produto a empresa %s: (1)SIM (0)NAO", industria[i].produto.detalhamento);
+                scanf("%d", &opcao);
 
-                if (opcao ==1){
+                if (opcao == 1)
+                {
                     printf("Qual o nome do produto:");
-                    fflush(stdin);
-                    scanf("%s",&industria[i].produto.detalhamento);
+                    scanf("%s", industria[i].produto.detalhamento);
 
                     printf("Qual o preco do produto: ");
-                    fflush(stdin);
-                    scanf("%f",industria[i].produto.preco);
+                    scanf("%f", &industria[i].produto.preco);
 
                     time_t t;
                     struct tm *infoTempo;
@@ -63,52 +62,93 @@ int main() {
                     time(&t);
                     infoTempo = localtime(&t);
 
-                    // Formatar a data atual como string e atribuir a dataCadastro
                     strftime(industria[i].produto.dataCadastro, sizeof(industria[i].produto.dataCadastro), "%Y-%m-%d %H:%M:%S", infoTempo);
-                    printf("Data do cadastro %s",industria[i].produto.dataCadastro);
-                    
-                }do{
+                    printf("Data do cadastro %s", industria[i].produto.dataCadastro);
+                }
+
+                do
+                {
                     printf("Deseja cadastrar mais alguma empresa: (1)Sim (0)Nao");
-                    scanf("%d",&sair);
-                } while (sair <0 || sair >1);
-                
-            } while (sair !=1); 
-            
-            
+                    scanf("%d", &sair);
+                } while (sair < 0 || sair > 1);
+
+                if (sair == 1)
+                {
+                    i++;
+                }
+            } while (sair != 0);
             break;
 
-            case 2:
+        case 2:
+            i = 0;
+            do
+            {
+                cadastrarConsumidor(&cliente[i]);
+                nCliente++;
+
+                printf("Deseja cadastrar uma compra ao cliente a empresa %s: (1)SIM (0)NAO", cliente[i].nome);
+                scanf("%d", &opcao);
+
+                if (opcao == 1)
+                {
+                    // Implemente a lógica para cadastrar compra
+                }
+
+                do
+                {
+                    printf("Deseja cadastrar mais algum cliente: (1)Sim (0)Nao");
+                    scanf("%d", &sair);
+                } while (sair < 0 || sair > 1);
+
+                if (sair == 1)
+                {
+                    i++;
+                }
+            } while (sair != 0);
             break;
 
-            case 3:
-
+        case 3:
+            // Implemente a lógica para listar tipos de serviços
             break;
 
-            case 4:
+        case 4:
+            // Implemente a lógica para listar clientes
             break;
 
-            case 5:
+        case 5:
+            // Implemente a lógica para listar prestadores de serviço
             break;
 
-            case 6:
+        case 6:
+            // Implemente a lógica para listar clientes de estado específico
             break;
 
-            case 7:
+        case 7:
+            // Implemente a lógica para listar prestadores de serviço de tipo específico
             break;
 
-            case 8: 
+        case 8:
+            // Implemente a lógica para encontrar o estado com o serviço mais caro no país
             break;
 
-            case 9: 
+        case 9:
+            // Implemente a lógica para listar serviços em ordem crescente
             break;
 
-            default:
+        case 10:
+            // Implemente a lógica para listar nomes de clientes em ordem crescente
+            break;
 
-            printf ("Digite um numero valido!");
+        case 11:
+            break;
 
+        default:
+            printf("Digite um numero valido!\n");
         }
 
-    } while (opcao != 0);
+    } while (opcao != 11);
 
-        printf("\n\nPrograma encerrado!");
+    printf("\n\nPrograma encerrado!\n");
+
+    return 0;
 }
